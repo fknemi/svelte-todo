6decddceb0dc958076867271b2a91f1d2fc467ff
fknemi <vihaan.s310@gmail.com>
2024-07-07 17:50:00 +0530

    idk

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,77 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import Task from "./task.svelte";
+
+    let tasks = [
+        { id: "101", title: "Task #101", completed: false },
+        { id: "102", title: "Task #102", completed: false },
+        { id: "103", title: "Task #103", completed: false },
+        { id: "104", title: "Task #104", completed: false },
+        { id: "105", title: "Task #105", completed: false },
+        { id: "106", title: "Task #106", completed: false },
+    ];
+    let taskTitle = "";
+
+    function onSubmit(e) {
+        e.preventDefault();
+        if (taskTitle.trim()) {
+            tasks = [
+                ...tasks,
+                {
+                    id: Date.now().toString(),
+                    title: taskTitle,
+                    completed: false,
+                },
+            ];
+            taskTitle = ""; // Clear the input after submission
+        }
+    }
+</script>
+
+<main>
+    {#each tasks as task (task.id)}
+        <Task id={task.id} title={task.title} completed={task.completed} />
+    {/each}
+    <input bind:value={taskTitle} on:change={onSubmit} />
+</main>
+
+<style>
+    @import url("https://fonts.googleapis.com/css2?family=Open+Sans:wght@300..800&display=swap");
+
+    *,
+    *::after,
+    *::before {
+        font-family: "Open Sans", sans-serif;
+        font-weight: 450;
+        margin: 0;
+        padding: 0;
+        transition: all 0.5s;
+    }
+
+    :global(html, body) {
+        overflow-x: hidden;
+        height: 100vh;
+        width: 100vh;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        margin-left: 30vh;
+    }
+
+    main {
+        border-radius: 8px;
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+        justify-content: center;
+        gap: 2rem;
+    }
+    input {
+        width: 20rem;
+        height: 2rem;
+        outline: none;
+        border: 2px solid #1111;
+        border-radius: 8px;
+        justify-self: center;
+        margin-left: -10%;
+    }
+</style>
---
+++ src/routes/task.svelte
@@ -0,0 +1,93 @@
+<script>
+    export let id;
+    export let title;
+    export let completed;
+</script>
+
+<div class={`task ${completed ? "checked" : ""}`}>
+    <div class="checkbox">
+        <input type="checkbox" {id} bind:checked={completed} />
+        <label for={id}></label>
+    </div>
+    <span>{title}</span>
+</div>
+
+<style>
+    .task {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        font-size: 1.5rem;
+        gap: 2rem;
+    }
+
+    .checkbox {
+        width: 2rem;
+        height: 2rem;
+        background-color: #f4f4f4;
+        border: 2px solid rgba(0, 0, 0, 0.4);
+        border-radius: 8px;
+        display: inline-flex;
+        position: relative;
+        justify-content: center;
+        align-items: center;
+        overflow: hidden;
+        transition:
+            box-shadow 0.3s,
+            transform 0.3s;
+    }
+
+    .checkbox input[type="checkbox"] {
+        cursor: pointer;
+        width: 100%;
+        height: 100%;
+        border: none;
+        outline: none;
+        -webkit-appearance: none;
+        -moz-appearance: none;
+        appearance: none;
+        position: absolute;
+        top: 0;
+        left: 0;
+        opacity: 0;
+    }
+
+    .checkbox label {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        width: 80%;
+        height: 80%;
+        background-color: transparent;
+        border-radius: inherit;
+        border: 2px solid transparent;
+    }
+
+    .checkbox:hover {
+        box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.2);
+        transform: scale(1.05);
+    }
+
+    .checkbox input[type="checkbox"]:checked {
+        text-decoration: line-through;
+        color: rgba(0, 0, 0, 0.4);
+    }
+    .checked span {
+        text-decoration: line-through;
+        color: rgba(0, 0, 0, 0.4);
+    }
+
+    .checkbox input[type="checkbox"]:checked + label::after {
+        content: "";
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        width: 50%;
+        height: 50%;
+        border-bottom: 3px solid #000;
+        border-right: 3px solid #000;
+        opacity: 0.7;
+    }
+</style>
